c6b51c29ab6e4423a2bfb72673e9e8a819422cad
Matheus H. de Souza <matheussouza@aiqfome.com>
2020-05-07 22:30:08 -0300

    Limpando templade do demo

--- app/src/main/java/com/aiqfome/aiqchartdemo/ui/MainActivity.java
+++ app/src/main/java/com/aiqfome/aiqchartdemo/ui/MainActivity.java
@@ -32,19 +32,12 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_gallery)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     @Override

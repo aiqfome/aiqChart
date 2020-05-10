761e9651dc9e6181eb76875a3bea5e80e014e831
Matheus H. de Souza <matheussouza@aiqfome.com>
2020-05-10 18:34:45 -0300

    Adicionado exemplo de gráficos de barras

--- app/src/main/java/com/aiqfome/aiqchartdemo/ui/bar_chart/BarChartFragment.java
+++ app/src/main/java/com/aiqfome/aiqchartdemo/ui/bar_chart/BarChartFragment.java
@@ -1,11 +1,11 @@
 package com.aiqfome.aiqchartdemo.ui.bar_chart;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.aiqfome.aiqchart.animation.Animation;
 import com.aiqfome.aiqchart.model.BarSet;
 import com.aiqfome.aiqchartdemo.R;
 import com.aiqfome.aiqchartdemo.databinding.FragmentBarChartBinding;
@@ -34,26 +35,45 @@ public class BarChartFragment extends Fragment {
         binding.setLifecycleOwner(this);
 
         loadBarChartOne();
+        loadBarChartTwo();
 
         return binding.getRoot();
     }
 
     private void loadBarChartOne() {
-
         final String[] mLabels = {"A", "B", "C", "D"};
         final float[] mValues = {6.5f, 8.5f, 2.5f, 10f};
 
         BarSet barSet = new BarSet(mLabels, mValues);
         barSet.setColor(this.getResources().getColor(R.color.colorPrimary));
 
-        binding.layoutCharts.barChartOne.reset();
-        binding.layoutCharts.barChartOne.addData(barSet);
+        binding.layoutBarChartOne.barChartOne.reset();
+        binding.layoutBarChartOne.barChartOne.addData(barSet);
 
-        binding.layoutCharts.barChartOne.setLabelsFormat(new DecimalFormat("#"));
+        binding.layoutBarChartOne.barChartOne.setLabelsFormat(new DecimalFormat("#"));
 
-        com.aiqfome.aiqchart.animation.Animation anim = new com.aiqfome.aiqchart.animation.Animation(750);
+        Animation anim = new Animation(750);
         anim.setInterpolator(new AccelerateDecelerateInterpolator());
-        binding.layoutCharts.barChartOne.show(anim);
+        binding.layoutBarChartOne.barChartOne.show(anim);
+
+        binding.executePendingBindings();
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void loadBarChartTwo() {
+        final String[] mLabels = {"", "", "", "", "", "", "", "", "", "", "", "", "", "",
+                "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+
+        final float[] mValues = {2.5f, 3.7f, 4f, 8f, 4.5f, 4f, 5f, 7f, 10f, 14f, 12f, 6f, 7f, 8f,
+                9f, 3f, 4f, 5f, 6f, 7f, 8f, 9f, 11f, 12f, 14, 13f, 10f, 9f, 8f, 7f, 6f};
+
+        BarSet barSet = new BarSet(mLabels, mValues);
+        barSet.setColor(this.getResources().getColor(R.color.colorPrimary));
+        binding.layoutBarChartTwo.barChartTwo.addData(barSet);
+        binding.layoutBarChartTwo.barChartTwo.setLabelsFormat(new DecimalFormat("#"));
+
+        binding.layoutBarChartTwo.barChartTwo.show(new Animation().
+                setInterpolator(new AccelerateDecelerateInterpolator()));
 
         binding.executePendingBindings();
     }

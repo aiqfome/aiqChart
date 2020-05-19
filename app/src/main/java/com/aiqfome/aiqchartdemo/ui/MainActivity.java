d3720712af434b14efd3c03930bdbe6457e9ed79
Lucas Villa Verde <lucasgabrielvverde@gmail.com.br>
2020-05-18 21:58:02 -0300

    Main Activity databinding

--- app/src/main/java/com/aiqfome/aiqchartdemo/ui/MainActivity.java
+++ app/src/main/java/com/aiqfome/aiqchartdemo/ui/MainActivity.java
@@ -2,38 +2,35 @@ package com.aiqfome.aiqchartdemo.ui;
 
 import android.os.Bundle;
 
-import com.aiqfome.aiqchartdemo.R;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.aiqfome.aiqchartdemo.R;
+import com.aiqfome.aiqchartdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding bindingMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        bindingMain = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(bindingMain.appBarLayout.toolbar);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_bar_chart)
-                .setDrawerLayout(drawer)
+                .setDrawerLayout(bindingMain.drawerLayout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bindingMain.navView, navController);
     }
 
     @Override

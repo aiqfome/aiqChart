7e6ca8503ef08738af251561dd9c7942cb817f44
Lucas Villa Verde <lucasgabrielvverde@gmail.com.br>
2020-05-18 22:02:00 -0300

    Home fragment databinding.

--- app/src/main/java/com/aiqfome/aiqchartdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/aiqfome/aiqchartdemo/ui/home/HomeFragment.java
@@ -4,31 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.aiqfome.aiqchartdemo.R;
+import com.aiqfome.aiqchartdemo.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                binding.textHome.setText(s);
             }
         });
-        return root;
+        return binding.getRoot();
     }
 }
